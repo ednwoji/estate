5ee449095284cdf25d47d650b9f64721a1617e65
Emeka Nwoji <ednwoji@unionbankng.com>
2023-06-04 10:24:13 +0100

    Final changes before deployment

--- src/main/java/com/Estateapp/estate/EstateApplication.java
+++ src/main/java/com/Estateapp/estate/EstateApplication.java
@@ -32,48 +32,6 @@ public class EstateApplication {
 		SpringApplication.run(EstateApplication.class, args);
 
 
-		String inputDate = "1999-10-06";
-
-		// Parse the input date
-		LocalDate date = LocalDate.parse(inputDate);
-
-		// Format the date to the desired output format
-		DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy", Locale.ENGLISH);
-		String formattedDate = date.format(outputFormatter).toUpperCase();
-
-		System.out.println("Formatted date: " + formattedDate);
-
-//		String prompt = "Who is president of Nigeria?";
-//
-//		CloseableHttpClient httpClient = HttpClients.createDefault();
-//
-//		HttpPost httpPost = new HttpPost("https://api.openai.com/v1/engines/text-davinci-003/completions");
-//
-//		httpPost.setHeader("Content-Type", "application/json");
-//		httpPost.setHeader("Authorization", "Bearer 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");
-//
-//		StringEntity requestEntity = new StringEntity("{\"prompt\":\"" + prompt + "\",\"max_tokens\":1,\"temperature\":0,\"n\":1,\"stop\":\"\\n\"}");
-//		httpPost.setEntity(requestEntity);
-//
-//		CloseableHttpResponse response = httpClient.execute(httpPost);
-//		try {
-//			HttpEntity responseEntity = response.getEntity();
-//			String responseBody = EntityUtils.toString(responseEntity);
-//			System.out.println(responseBody);
-//		} finally {
-//			response.close();
-//		}
-
-
-//		File journalDirectory = new File("C:\\u01\\app\\oracle\\shared\\properties\\Terminal");
-////		File footageDirectory = new File(path+Upload.getBranch()+"\\"+Upload.getTerminal()+"\\"+"Footages");
-//
-//		if (!journalDirectory.exists()) {
-//			boolean status = journalDirectory.mkdir();
-//		}
-//
-//		Set<Character> matchedChars = new HashSet<>();
-
 	}
 
 }
